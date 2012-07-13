44099980ee2c0033792d6ef37709a4ee1766a827
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-07-13 12:12:29 -0400

    Update to the precise release

--- ubuntu/repositories.sh
+++ ubuntu/repositories.sh
@@ -2,6 +2,6 @@
 # - Add partner sources
 # - Update new sources
 # - Run upgrades
-sudo sh -c "echo 'deb http://archive.canonical.com/ubuntu natty partner\ndeb-src http://archive.canonical.com/ubuntu natty partner' > /etc/apt/sources.list.d/partners.sources.list"
+sudo sh -c "echo 'deb http://archive.canonical.com/ubuntu precise partner\ndeb-src http://archive.canonical.com/ubuntu precise partner' > /etc/apt/sources.list.d/partners.sources.list"
 sudo apt-get update
 sudo apt-get -y upgrade
