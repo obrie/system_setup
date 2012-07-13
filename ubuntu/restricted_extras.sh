fe000cf3c47fad7d9fd97b02d3f95762480e7117
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-07-13 13:20:11 -0400

    Fix ubuntu-restricted-extras install causing an interactive prompt for the ttf-mscorefonts-installer package

--- ubuntu/restricted_extras.sh
+++ ubuntu/restricted_extras.sh
@@ -1,2 +1,3 @@
 # Restricted extras
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
 sudo apt-get install -y ubuntu-restricted-extras --quiet
