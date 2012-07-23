3e4e8ed67bde11b2c883c5c391eee9cdfa6ebd64
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-07-23 13:46:29 -0400

    Update project binary names

--- ubuntu/projects-binaries.sh
+++ ubuntu/projects-binaries.sh
@@ -1,7 +1,7 @@
 source ./ubuntu/config.sh
 source $HOME/.rvm/scripts/rvm
 
-# Viximo binaries
+# Project binaries
 cat >> $HOME/.profile <<EOF
 
 if [ -d "$PROJECTS/bin" ] ; then
@@ -11,27 +11,23 @@ EOF
 
 rm -rf $PROJECTS/bin/*
 
-# vreload - Reloads the app in the current directory
-echo -en 'touch tmp/restart.txt' > $PROJECTS/bin/vreload
-chmod 755 $PROJECTS/bin/vreload
-
-# vtail - Tail all application logs
+# xtail - Tail all application logs
 # - Apache by default
-cat > $PROJECTS/bin/vtail <<EOF
+cat > $PROJECTS/bin/xtail <<EOF
 if which adb >/dev/null; then
   trap "kill 0" SIGINT SIGTERM EXIT
   adb logcat > /tmp/logcat &
 fi
 tail -f $PROJECTS/*/log/development.log /var/log/apache2/access.log /var/log/apache2/error.log /tmp/logcat
 EOF
-chmod 755 $PROJECTS/bin/vtail
+chmod 755 $PROJECTS/bin/xtail
 
-# vthin - Runs a thin server
-echo 'bundle exec thin start -p $1 -s $2' > $PROJECTS/bin/vthin
-chmod 755 $PROJECTS/bin/vthin
+# xthin - Runs a thin server
+echo 'bundle exec thin start -p $1 -s $2' > $PROJECTS/bin/xthin
+chmod 755 $PROJECTS/bin/xthin
 
-# vandroid - Android helpers
-cat > $PROJECTS/bin/vandroid <<EOF
+# xandroid - Android helpers
+cat > $PROJECTS/bin/xandroid <<EOF
 if [ "\$1" = "launch" ]; then
   if test -z "\$2"; then
     AVD="default"
@@ -42,13 +38,13 @@ if [ "\$1" = "launch" ]; then
   emulator -avd \$AVD &
 elif [ "\$1" = "install" ]; then
   if test -z "\$2"; then
-    APK="$PROJECTS/android-test/android-app/target/*.apk"
+    APK="$PROJECTS/AndroidTestAppHere/target/*.apk"
   else
     APK="\$2"
   fi
 
-  cd $PROJECTS/libraries/ViximoAndroid && git pull && mvn clean && mvn install &&\
-  cd $PROJECTS/android-test/android-app && git pull && mvn clean && mvn package &&\
+  cd $PROJECTS/AndroidLibHere && git pull && mvn clean && mvn install &&\
+  cd $PROJECTS/AndroidTestAppHere && git pull && mvn clean && mvn package &&\
   adb install -r \$APK
 fi
 EOF
