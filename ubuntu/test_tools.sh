55e3dca69055b68f31dbff5c4860dfe2ebdf8394
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-07-23 12:02:16 -0400

    Use apt for installing phantomjs instead of using a custom script

--- ubuntu/test_tools.sh
+++ ubuntu/test_tools.sh
@@ -1,9 +1,4 @@
 # Test tools
 
 # PhantomJS
-sudo apt-get install libqt4-dev libqtwebkit-dev qt4-qmake
-git clone git://github.com/ariya/phantomjs.git /tmp/phantomjs && cd /tmp/phantomjs
-git checkout 1.3.0
-qmake && make
-sudo mv bin/phantomjs /usr/local/bin/
-rm -rf /tmp/phantomjs
+sudo apt-get install -y phantomjs
