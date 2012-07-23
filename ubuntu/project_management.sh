d3129139572fce52f25a78cb612924229379e688
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-07-23 11:42:01 -0400

    Update java home for project management env vars

--- ubuntu/project_management.sh
+++ ubuntu/project_management.sh
@@ -5,7 +5,7 @@ tar -xzvf /tmp/maven.tar.gz
 sudo mv apache-maven-3.0.4 /var/local/apache-maven
 sudo sh -c "cat > /etc/environment <<EOF
 PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/var/local/apache-maven/bin\"
-JAVA_HOME=\"/usr/lib/jvm/java-6-sun-1.6.0.26\"
+JAVA_HOME=\"/usr/lib/jvm/java-7-oracle\"
 M2_HOME=\"/var/local/apache-maven\"
 MAVEN_HOME=\"/var/local/apache-maven\"
 M2=\"/var/local/apache-maven/bin\"
