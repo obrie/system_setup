297985b4cb15242b92e71f33d508687b5449121c
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-07-24 13:12:30 -0400

    Add imagemagick

--- ubuntu/graphics.sh
+++ ubuntu/graphics.sh
@@ -5,5 +5,5 @@
 # - KolourPaint
 sudo apt-get install -y compizconfig-settings-manager
 sudo apt-get install -y gimp
-sudo apt-get install -y graphicsmagick graphviz libmagick++-dev
+sudo apt-get install -y imagemagick graphicsmagick graphviz libmagick++-dev
 sudo apt-get install -y kolourpaint4
