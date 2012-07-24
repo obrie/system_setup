6412a85aea16333d1b0287fba6b09fe5588f4091
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-07-24 11:10:33 -0400

    Fix phpmyadmin not getting set up correctly

--- ubuntu/databases.sh
+++ ubuntu/databases.sh
@@ -1,10 +1,14 @@
 # Database
 # - Memcached
 # - MySQL
+# - PHPMyAdmin
 # - Sqlline
 # - Sqlite
 sudo apt-get install -y libmemcached-dev memcached
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libmysqlclient-dev mytop mysql-server phpmyadmin
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libmysqlclient-dev mytop mysql-server
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y phpmyadmin
+sudo ln -sv /etc/phpmyadmin/apache.conf /etc/apache2/conf.d/phpmyadmin.conf
+sed '/^.*AllowNoPassword.*/ s/\/\/ //' /etc/phpmyadmin/config.inc.php > /tmp/conf.tmp && sudo mv /tmp/conf.tmp /etc/phpmyadmin/config.inc.php
 sudo apt-get install -y redis-server
 sudo apt-get install -y sqlline
 sudo apt-get install -y libsqlite3-dev sqlite3
