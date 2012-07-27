ca4e4de2bdbeab42e81ff76c8d332ea6a6f70480
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-07-27 14:13:17 -0400

    Add shutter

--- ubuntu/media.sh
+++ ubuntu/media.sh
@@ -2,6 +2,8 @@
 # - PulseAudio control
 # - Sound conversion
 # - Video conversion
+# - Screenshots
 sudo apt-get install -y paprefs pavucontrol pulseaudio-module-raop
 sudo apt-get install -y soundconverter winff
 sudo apt-get install -y ffmpeg vlc
+sudo apt-get install -y shutter
