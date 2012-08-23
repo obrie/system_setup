caa9114951859aa601e62dcefb5f5f59839fc9c4
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-08-23 14:49:51 -0400

    Add spotify

--- ubuntu/media.sh
+++ ubuntu/media.sh
@@ -3,7 +3,13 @@
 # - Sound conversion
 # - Video conversion
 # - Screenshots
+# - Spotify
 sudo apt-get install -y paprefs pavucontrol pulseaudio-module-raop
 sudo apt-get install -y soundconverter winff
 sudo apt-get install -y ffmpeg vlc
 sudo apt-get install -y shutter
+
+sudo sh -c "echo 'deb http://repository.spotify.com stable non-free' > /etc/apt/sources.list.d/spotify.sources.list"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+sudo apt-get update
+sudo apt-get install -y spotify-client
