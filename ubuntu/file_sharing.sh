c83f4bf284af51071424a5f3de05811c17166e85
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-09-05 18:47:27 -0400

    Add dropbox, googlecl

--- ubuntu/file_sharing.sh
+++ ubuntu/file_sharing.sh
@@ -1,3 +1,9 @@
 # File sharing
 # - SSH
 sudo apt-get install -y openssh-server
+
+# - Dropbox
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
+sudo add-apt-repository "deb http://linux.dropbox.com/ubuntu $(lsb_release -sc) main"
+sudo apt-get update
+sudo apt-get install -y nautilus-dropbox
--- ubuntu/media.sh
+++ ubuntu/media.sh
@@ -1,15 +1,21 @@
 # Media
 # - PulseAudio control
-# - Sound conversion
-# - Video conversion
-# - Screenshots
-# - Spotify
 sudo apt-get install -y paprefs pavucontrol pulseaudio-module-raop
+
+# - Sound conversion
 sudo apt-get install -y soundconverter winff
+
+# - Video conversion
 sudo apt-get install -y ffmpeg vlc
+
+# - Screenshots
 sudo apt-get install -y shutter
 
+# - Spotify
 sudo sh -c "echo 'deb http://repository.spotify.com stable non-free' > /etc/apt/sources.list.d/spotify.sources.list"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 sudo apt-get update
 sudo apt-get install -y spotify-client
+
+# - Google CL
+sudo apt-get install -y googlecl
