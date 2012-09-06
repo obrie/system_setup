fd953ac6a5d8abb8126db62e6cf38d65275154b4
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-09-06 15:48:09 -0400

    Use add-apt-repository instead of writing manually to /etc/apt/...

--- ubuntu/repositories.sh
+++ ubuntu/repositories.sh
@@ -2,6 +2,6 @@
 # - Add partner sources
 # - Update new sources
 # - Run upgrades
-sudo sh -c "echo 'deb http://archive.canonical.com/ubuntu precise partner\ndeb-src http://archive.canonical.com/ubuntu precise partner' > /etc/apt/sources.list.d/partners.sources.list"
+sudo add-apt-repository -y "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
 sudo apt-get update
 sudo apt-get -y upgrade
