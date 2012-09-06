0e4a7453c33daf655737e57c0c876e5f89ef196f
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-09-06 15:46:58 -0400

    Fix dropbox add-apt-repository prompt

--- ubuntu/file_sharing.sh
+++ ubuntu/file_sharing.sh
@@ -4,6 +4,6 @@ sudo apt-get install -y openssh-server
 
 # - Dropbox
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
-sudo add-apt-repository "deb http://linux.dropbox.com/ubuntu $(lsb_release -sc) main"
+sudo add-apt-repository -y "deb http://linux.dropbox.com/ubuntu $(lsb_release -sc) main"
 sudo apt-get update
 sudo apt-get install -y nautilus-dropbox
