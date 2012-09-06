2c34db99faefb518c9bf59b31d2dac038b88c97c
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-09-06 15:46:25 -0400

    Fix folder not being created properly before writing to it

--- ubuntu/code_tools.sh
+++ ubuntu/code_tools.sh
@@ -1,11 +1,9 @@
-# Code search
+# Code tools
 # - Ack
 sudo apt-get install -y ack-grep
 sudo ln -s /usr/bin/ack-grep /usr/bin/ack
 
-# Text Editing
-# - sublime2
-# - gnote
+# - Sublime Text 2
 wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.tar.bz2 -O /tmp/sublime.tar.bz2
 bunzip2 /tmp/sublime.tar.bz2
 tar -xvf /tmp/sublime.tar
@@ -30,6 +28,7 @@ Name=New Window
 Exec=sublime -n
 TargetEnvironment=Unity
 "
+mkdir -p $HOME/.local/share/applications
 sh -c "cat > $HOME/.local/share/applications/defaults.list <<EOF
 [Default Applications]
 application/x-perl=sublime.desktop
@@ -42,9 +41,11 @@ text/mathml=sublime.desktop
 text/x-python=sublime.desktop
 text/x-sql=sublime.desktop
 "
+
+# - GNote
 sudo apt-get install -y gnote
 
-# Mime database
+# - Mime database
 wget https://github.com/janlelis/rubybuntu-mime/zipball/master -O /tmp/rubybuntu-mime.zip
 unzip /tmp/rubybuntu-mime.zip -d /tmp/rubybuntu-mime
 rm /tmp/rubybuntu-mime.zip
@@ -52,7 +53,7 @@ sudo cp /tmp/rubybuntu-mime/*/*.xml /usr/share/mime/packages
 rm -rf /tmp/rubybuntu-mime
 sudo update-mime-database /usr/share/mime
 
-# Eclipse
+# - Eclipse
 sudo apt-get install -y eclipse
 sudo sh -c "cat >> /etc/eclipse.ini <<EOF
 -XX:PermSize=128m
