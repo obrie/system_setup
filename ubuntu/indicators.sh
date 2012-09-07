372b6f1972cdca89cb1616ad585679c95ff65abd
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-09-07 08:36:04 -0400

    Add sysmonitor's autostart script to indicators script

--- ubuntu/indicators.sh
+++ ubuntu/indicators.sh
@@ -3,6 +3,19 @@
 sudo add-apt-repository -y ppa:alexeftimie/ppa
 sudo apt-get update
 sudo apt-get install -y indicator-sysmonitor
+mkdir -p $HOME/.config/autostart
+cat > $HOME/.config/autostart/sysmonitor.desktop <<EOF
+[Desktop Entry]
+Type=Application
+Exec=indicator-sysmonitor
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name[en_US]=sysmonitor
+Name=sysmonitor
+Comment[en_US]=System Monitor
+Comment=System Monitor
+EOF
 
 # - Gmail
 sudo apt-get install -y gm-notify
