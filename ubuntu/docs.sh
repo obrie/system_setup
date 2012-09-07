bb93f5bd399bf61672613a30ba07804c728c7d01
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-09-07 08:35:41 -0400

    Add gnote and its autostart script to docs script

--- ubuntu/docs.sh
+++ ubuntu/docs.sh
@@ -5,3 +5,19 @@ sudo apt-get install -y lo-menubar
 # - PDF Reader
 echo "acroread-common acroread-common/default-viewer boolean true" | sudo debconf-set-selections
 sudo apt-get install -y acroread
+
+# - GNote
+sudo apt-get install -y gnote
+mkdir -p $HOME/.config/autostart
+cat > $HOME/.config/autostart/gnote.desktop <<EOF
+[Desktop Entry]
+Type=Application
+Exec=gnote
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name[en_US]=gnote
+Name=gnote
+Comment[en_US]=Gnote
+Comment=Gnote
+EOF
