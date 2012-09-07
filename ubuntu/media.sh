2e55036bdbad3ab30bf12d1aeced7aad8ba373f8
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-09-07 08:36:39 -0400

    Use add-apt-repository for spotify repo instead of manually adding apt source files

--- ubuntu/media.sh
+++ ubuntu/media.sh
@@ -12,7 +12,7 @@ sudo apt-get install -y ffmpeg vlc
 sudo apt-get install -y shutter
 
 # - Spotify
-sudo sh -c "echo 'deb http://repository.spotify.com stable non-free' > /etc/apt/sources.list.d/spotify.sources.list"
+sudo add-apt-repository -y "deb http://repository.spotify.com stable non-free"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 sudo apt-get update
 sudo apt-get install -y spotify-client
