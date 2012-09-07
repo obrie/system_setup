5b6fdf5676946d3613a6d2f9d574a9b9751d1d6d
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-09-07 08:37:05 -0400

    Fix hard-coded ubuntu release / virtualbox version in virtualbox install

--- ubuntu/virtualization.sh
+++ ubuntu/virtualization.sh
@@ -2,8 +2,8 @@ source ./ubuntu/config.sh
 
 # Virtualization
 # - Virtualbox
-sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian precise contrib' > /etc/apt/sources.list.d/virtualbox.sources.list"
+sudo add-apt-repository -y "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib"
 wget -q -O - http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y virtualbox-4.1
+sudo apt-get install -y $(grep -hioRE "virtualbox-[0-9.]+" /var/lib/apt/lists/download.virtualbox.org_virtualbox* | tail -1)
 sudo adduser $USER vboxusers
