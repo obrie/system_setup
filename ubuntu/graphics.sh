7d7caf6c4c88f315405ad2c1bff8d665211e6e93
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-09-11 18:20:18 -0400

    Install libmagickcore-dev / libmagickwand-dev, not libmagick++-dev

--- ubuntu/graphics.sh
+++ ubuntu/graphics.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y compizconfig-settings-manager
 sudo apt-get install -y gimp
 
 # - GraphicsMagick
-sudo apt-get install -y imagemagick graphicsmagick graphviz libmagick++-dev
+sudo apt-get install -y imagemagick graphicsmagick graphviz libmagickcore-dev libmagickwand-deev
 
 # - KolourPaint
 sudo apt-get install -y kolourpaint4
